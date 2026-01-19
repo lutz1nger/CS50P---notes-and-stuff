bde26dd8e43eb1882da3190545d922ffbaa5101a
lutz1nger <christian.lutz91@gmx.de>
2026-01-19 17:39:31 +0100

    Update notes_0.py

--- Lecture 0 - Functions, Variables/notes_0.py
+++ Lecture 0 - Functions, Variables/notes_0.py
@@ -6,7 +6,7 @@
 #FUNCTIONS:
 #commands performing actions - called SIDE EFFECTS; e.g.
 
-print() #-> prints input on the screen or display
+print() #prints input on the screen or display
 
 #inside parantheses is the input - called ARGUMENTS
 
@@ -18,7 +18,7 @@ name = 'David'
 #multiple values can be assigned in one line, e.g.
 
 name = 'David Malan'              
-first_name, last_name = name.split(' ') #-> output: first_name is 'David' and last_name is 'Malan'
+first_name, last_name = name.split(' ') #output: first_name is 'David' and last_name is 'Malan'
 
 #COMMENTS:
 #inside a code comments can be made using '#', e.g. #Print the output
@@ -26,12 +26,12 @@ first_name, last_name = name.split(' ') #-> output: first_name is 'David' and la
 
 #print():
 
-print('hello' + 'world') #-> output: helloworld
-print('hello', 'world')  #-> output: hello world
+print('hello' + 'world') #output: helloworld
+print('hello', 'world')  #output: hello world
 
 #CHECK ALWAYS THE FUNCTION DOCUMENTATION!!! e.g. for print():
 
-print(*objects, sep=' ', end='\n', file=None, flush=False) #->* before objects means that multiple arguments can be used as input, default seperation is a whitespace (sep=' '), default end is a new line (end='\n')
+print(*objects, sep=' ', end='\n', file=None, flush=False) #* before objects means that multiple arguments can be used as input, default seperation is a whitespace (sep=' '), default end is a new line (end='\n')
 
 #objects, sep, end, ... are called PARAMETERS -> difference arguments and parameters: parameters are defined in the function definition and arguments are values being passed to the function
 
@@ -39,16 +39,16 @@ print(*objects, sep=' ', end='\n', file=None, flush=False) #->* before objects m
 #string variable -> text
 #str can be modifided by STRING-METHODS, e.g.
 
-str.strip() #-> whitspaces from the left and right of the variable are removed
+str.strip() #whitspaces from the left and right of the variable are removed
 
 #string-methdos can be linked multiple times, e.g.
 
-str.strip().title() #-> whitspaces from the left and right of the variable are removed and the first letter of every word is capitalized
+str.strip().title() #whitspaces from the left and right of the variable are removed and the first letter of every word is capitalized
 
 #F STRING:
 #formatting string, e.g.
 
-f_string = f'hello, {name}' #-> the f indidactes it is a f string and therefore, the variable name is used in this string
+f_string = f'hello, {name}' #the f indidactes it is a f string and therefore, the variable name is used in this string
 
 #INT:
 #int variable -> integers
@@ -61,14 +61,14 @@ f_string = f'hello, {name}' #-> the f indidactes it is a f string and therefore,
 #float can be rounded by the function round() -> documentation: round(number[, ndigits]) -> in documentations [] means the argument is optional
 #f string for float:
 
-float = f'{z:.2f}' #-> variable z is separated by a decimal point and has two decimal places
+float = f'{z:.2f}' #variable z is separated by a decimal point and has two decimal places
 
 #DEF:
 #own function can be created with the def function:
 
-def squared(x): #-> a function 'squared' is defined with the parameter x
-  y = x*x       #-> side effect, but this values are not returned by the function
-  return y      #-> RETURN VALUE, the value is returned by the function
+def squared(x): #a function 'squared' is defined with the parameter x
+  y = x*x       #side effect, but this values are not returned by the function
+  return y      #RETURN VALUE, the value is returned by the function
                 
 #parameters are optional (e.g. def squared():)
 #default values can be added to the parameters (e.g. def squared(x=3):)
@@ -78,20 +78,20 @@ def squared(x): #-> a function 'squared' is defined with the parameter x
 x = 2
 def multiple(y):
   global x
-  result1 = y + x  #-> here is x = 2
-  x = 3            #-> here x gets 3
+  result1 = y + x  #here is x = 2 as defined
+  x = 3            #here x gets 3 as assigned
   result2 = y + x
   return result1, result2             
             
 #def must be defined before used in the code, therefore, one option is to work with a main code:
 
-def main():      #-> here the main code is written as def
+def main():      #here the main code is written as def
   ....
   
-def function1(): #-> then other functions are defined below teh main code
+def function1(): #then other functions are defined below teh main code
   ....
 
-main()           #-> main code is executed
+main()           #main code is executed
 
 #COMMANDS IN THE TERMINAL:
 
